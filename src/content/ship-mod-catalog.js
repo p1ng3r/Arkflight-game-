4523eb7de70bf8c4f06cb265c54e2fe7ebe19805
p1ng3r <thep1ng3r@gmail.com>
2026-08-31 18:23:09 -0700

    feat(ship): merge mythic ship mods into catalog

--- src/content/ship-mod-catalog.js
+++ src/content/ship-mod-catalog.js
@@ -1,9 +1,11 @@
 import { SHIP_MODS as BASE_SHIP_MODS } from "./ship-mods.js";
 import { EPIC_SHIP_MODS } from "./ship-mods-epic.js";
 import { LEGENDARY_SHIP_MODS } from "./ship-mods-legendary.js";
+import { MYTHIC_SHIP_MODS } from "./ship-mods-mythic.js";
 
 export const SHIP_MODS = Object.freeze({
   ...BASE_SHIP_MODS,
   ...EPIC_SHIP_MODS,
-  ...LEGENDARY_SHIP_MODS
+  ...LEGENDARY_SHIP_MODS,
+  ...MYTHIC_SHIP_MODS
 });
