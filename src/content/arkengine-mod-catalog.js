2ee7223229033c517754850595fa6dc91813c54c
p1ng3r <thep1ng3r@gmail.com>
2026-08-31 18:58:42 -0700

    feat: merge Mythic Arkengine Mods into canonical catalog

--- src/content/arkengine-mod-catalog.js
+++ src/content/arkengine-mod-catalog.js
@@ -2,10 +2,12 @@ import { ARKENGINE_MODS as STANDARD_ARKENGINE_MODS } from "./arkengine-mods.js";
 import { RARE_ARKENGINE_MODS } from "./arkengine-mods-rare.js";
 import { EPIC_ARKENGINE_MODS } from "./arkengine-mods-epic.js";
 import { LEGENDARY_ARKENGINE_MODS } from "./arkengine-mods-legendary.js";
+import { MYTHIC_ARKENGINE_MODS } from "./arkengine-mods-mythic.js";
 
 export const ARKENGINE_MODS = Object.freeze({
   ...STANDARD_ARKENGINE_MODS,
   ...RARE_ARKENGINE_MODS,
   ...EPIC_ARKENGINE_MODS,
-  ...LEGENDARY_ARKENGINE_MODS
+  ...LEGENDARY_ARKENGINE_MODS,
+  ...MYTHIC_ARKENGINE_MODS
 });
