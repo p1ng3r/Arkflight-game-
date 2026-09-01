14e66cbee12441ca418a4f23352b96f6c84168b1
p1ng3r <thep1ng3r@gmail.com>
2026-08-31 18:56:39 -0700

    feat: merge Legendary Arkengine Mods into canonical catalog

--- src/content/arkengine-mod-catalog.js
+++ src/content/arkengine-mod-catalog.js
@@ -1,9 +1,11 @@
 import { ARKENGINE_MODS as STANDARD_ARKENGINE_MODS } from "./arkengine-mods.js";
 import { RARE_ARKENGINE_MODS } from "./arkengine-mods-rare.js";
 import { EPIC_ARKENGINE_MODS } from "./arkengine-mods-epic.js";
+import { LEGENDARY_ARKENGINE_MODS } from "./arkengine-mods-legendary.js";
 
 export const ARKENGINE_MODS = Object.freeze({
   ...STANDARD_ARKENGINE_MODS,
   ...RARE_ARKENGINE_MODS,
-  ...EPIC_ARKENGINE_MODS
+  ...EPIC_ARKENGINE_MODS,
+  ...LEGENDARY_ARKENGINE_MODS
 });
