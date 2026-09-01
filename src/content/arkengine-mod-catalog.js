5939dd6433ecb67ccfa078d68571300976fd16ae
p1ng3r <thep1ng3r@gmail.com>
2026-08-31 18:54:11 -0700

    feat: merge Epic Arkengine Mods into canonical catalog

--- src/content/arkengine-mod-catalog.js
+++ src/content/arkengine-mod-catalog.js
@@ -1,7 +1,9 @@
 import { ARKENGINE_MODS as STANDARD_ARKENGINE_MODS } from "./arkengine-mods.js";
 import { RARE_ARKENGINE_MODS } from "./arkengine-mods-rare.js";
+import { EPIC_ARKENGINE_MODS } from "./arkengine-mods-epic.js";
 
 export const ARKENGINE_MODS = Object.freeze({
   ...STANDARD_ARKENGINE_MODS,
-  ...RARE_ARKENGINE_MODS
+  ...RARE_ARKENGINE_MODS,
+  ...EPIC_ARKENGINE_MODS
 });
