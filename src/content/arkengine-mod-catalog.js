fa83b2d764cd6d5c314313968a9282303af35df4
p1ng3r <thep1ng3r@gmail.com>
2026-09-03 14:00:20 -0700

    feat: attach art metadata to Arkengine mods

--- src/content/arkengine-mod-catalog.js
+++ src/content/arkengine-mod-catalog.js
@@ -3,11 +3,16 @@ import { RARE_ARKENGINE_MODS } from "./arkengine-mods-rare.js";
 import { EPIC_ARKENGINE_MODS } from "./arkengine-mods-epic.js";
 import { LEGENDARY_ARKENGINE_MODS } from "./arkengine-mods-legendary.js";
 import { MYTHIC_ARKENGINE_MODS } from "./arkengine-mods-mythic.js";
+import { withModArt } from "./mod-art.js";
 
-export const ARKENGINE_MODS = Object.freeze({
+const merged = {
   ...STANDARD_ARKENGINE_MODS,
   ...RARE_ARKENGINE_MODS,
   ...EPIC_ARKENGINE_MODS,
   ...LEGENDARY_ARKENGINE_MODS,
   ...MYTHIC_ARKENGINE_MODS
-});
+};
+
+export const ARKENGINE_MODS = Object.freeze(Object.fromEntries(
+  Object.entries(merged).map(([id, mod]) => [id, withModArt(mod, "arkengineMod")])
+));
