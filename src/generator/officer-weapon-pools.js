3ea0d7ec531ccb69c9dd237129b497a1a801f901
p1ng3r <thep1ng3r@gmail.com>
2026-09-06 05:23:59 -0700

    fix: use battlewatch officer weapon pools

--- src/generator/officer-weapon-pools.js
+++ src/generator/officer-weapon-pools.js
@@ -2,17 +2,17 @@ const DEFAULT_POOLS = Object.freeze({
   captain: Object.freeze(["rapier", "saber", "longsword"]),
   engineer: Object.freeze(["warhammer", "light-hammer", "maul"]),
   navigator: Object.freeze(["rapier", "shortsword", "spear"]),
-  watchmaster: Object.freeze(["crossbow", "longbow", "shortbow"]),
+  battlewatch: Object.freeze(["crossbow", "longbow", "shortbow"]),
   veilwarden: Object.freeze(["staff", "mace", "morningstar"])
 });
 
 const FACTION_POOLS = Object.freeze({
-  "grelkin cartel": Object.freeze({ captain:["battle-axe","falchion","longsword"], engineer:["warhammer","maul","pick"], navigator:["shortsword","spear","hatchet"], watchmaster:["crossbow","shortbow","longbow"], veilwarden:["staff","mace","flail"] }),
-  "brotherhood of the cosmic flame": Object.freeze({ captain:["longsword","spear","rapier"], engineer:["warhammer","light-hammer","mace"], navigator:["rapier","spear","shortsword"], watchmaster:["crossbow","longbow","spear"], veilwarden:["staff","mace","spear"] }),
-  freespacers: Object.freeze({ captain:["rapier","saber","shortsword"], engineer:["warhammer","light-hammer","club"], navigator:["rapier","shortsword","spear"], watchmaster:["longbow","crossbow","shortbow"], veilwarden:["staff","mace","spear"] }),
-  freebooters: Object.freeze({ captain:["saber","rapier","longsword"], engineer:["warhammer","light-hammer","maul"], navigator:["rapier","shortsword","spear"], watchmaster:["crossbow","longbow","shortbow"], veilwarden:["staff","mace","morningstar"] }),
-  "underwake syndicates": Object.freeze({ captain:["rapier","shortsword","dagger"], engineer:["light-hammer","warhammer","club"], navigator:["rapier","dagger","shortsword"], watchmaster:["crossbow","shortbow","dagger"], veilwarden:["staff","mace","dagger"] }),
-  "council of the void": Object.freeze({ captain:["spear","staff","rapier"], engineer:["warhammer","staff","mace"], navigator:["spear","staff","rapier"], watchmaster:["crossbow","longbow","spear"], veilwarden:["staff","mace","spear"] })
+  "grelkin cartel": Object.freeze({ captain:["battle-axe","falchion","longsword"], engineer:["warhammer","maul","pick"], navigator:["shortsword","spear","hatchet"], battlewatch:["crossbow","shortbow","longbow"], veilwarden:["staff","mace","flail"] }),
+  "brotherhood of the cosmic flame": Object.freeze({ captain:["longsword","spear","rapier"], engineer:["warhammer","light-hammer","mace"], navigator:["rapier","spear","shortsword"], battlewatch:["crossbow","longbow","spear"], veilwarden:["staff","mace","spear"] }),
+  freespacers: Object.freeze({ captain:["rapier","saber","shortsword"], engineer:["warhammer","light-hammer","club"], navigator:["rapier","shortsword","spear"], battlewatch:["longbow","crossbow","shortbow"], veilwarden:["staff","mace","spear"] }),
+  freebooters: Object.freeze({ captain:["saber","rapier","longsword"], engineer:["warhammer","light-hammer","maul"], navigator:["rapier","shortsword","spear"], battlewatch:["crossbow","longbow","shortbow"], veilwarden:["staff","mace","morningstar"] }),
+  "underwake syndicates": Object.freeze({ captain:["rapier","shortsword","dagger"], engineer:["light-hammer","warhammer","club"], navigator:["rapier","dagger","shortsword"], battlewatch:["crossbow","shortbow","dagger"], veilwarden:["staff","mace","dagger"] }),
+  "council of the void": Object.freeze({ captain:["spear","staff","rapier"], engineer:["warhammer","staff","mace"], navigator:["spear","staff","rapier"], battlewatch:["crossbow","longbow","spear"], veilwarden:["staff","mace","spear"] })
 });
 
 const HOUSE_TENDENCIES = Object.freeze({
