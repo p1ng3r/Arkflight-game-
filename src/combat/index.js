73a6fee83fd8f72b18c02a71db133baa914a07a2
p1ng3r <thep1ng3r@gmail.com>
2026-09-11 14:39:52 -0700

    refactor: drop retired system-damage export

--- src/combat/index.js
+++ src/combat/index.js
@@ -9,7 +9,6 @@ export * from "./engagement-rules.js";
 export * from "./station-action-state.js";
 export * from "./station-action-economy.js";
 export * from "./station-effect-rules.js";
-export * from "./system-damage.js";
 export * from "./weapon-combat.js";
 export * from "./weapon-targeting.js";
 export {
