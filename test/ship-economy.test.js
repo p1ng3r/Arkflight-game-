cbe3882549cc395568c266ead5e2df1d7a3791b8
p1ng3r <thep1ng3r@gmail.com>
2026-09-11 09:30:08 -0700

    test: lock supply gp value

--- test/ship-economy.test.js
+++ test/ship-economy.test.js
@@ -7,6 +7,7 @@ import {
   dailySupplyConsumption,
   salvageCargoUsage,
   supplyCargoUsage,
+  supplyGoldValue,
   zeroSupplyDayConsequences
 } from "../src/ship/ship-economy.js";
 
@@ -26,6 +27,14 @@ test("Supplies and Salvage Parts use the locked ten-to-one Cargo conversion", ()
   assert.equal(supplyCargoUsage(25), 2.5);
   assert.equal(salvageCargoUsage(10), 1);
   assert.equal(salvageCargoUsage(3), 0.3);
+});
+
+test("Supplies have the locked PF2e-facing base value of 1 gp each", () => {
+  assert.equal(SHIP_ECONOMY.supplyGpValue, 1);
+  assert.equal(supplyGoldValue(0), 0);
+  assert.equal(supplyGoldValue(1), 1);
+  assert.equal(supplyGoldValue(10), 10);
+  assert.equal(supplyGoldValue(25), 25);
 });
 
 test("uninstalled Mods use slot cost and weapons use authored Cargo value", () => {
